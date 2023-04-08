6b58ab894d9760c8751f0c6640ca4febb431ce9e
Pratik Gaikwad <pratikgaikwad1004@gmail.com>
2023-04-09 01:43:52 +0530

    Internet screen added

--- src/screens/InternetScreen.js
+++ src/screens/InternetScreen.js
@@ -1,12 +1,31 @@
-import { View, Text } from 'react-native'
+import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const InternetScreen = () => {
+
   return (
-    <View>
-      <Text>InternetScreen</Text>
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Icon name='wifi-off' size={100} color={"black"} />
+        <Text style={styles.text}>Internet is not Working. </Text>
+        <Button>Reload</Button>
+      </View>
+    </SafeAreaView>
   )
 }
 
 export default InternetScreen
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100%"
+  },
+  text: {
+    color: "black",
+    fontSize: 20,
+    marginTop: 20
+  }
+})
